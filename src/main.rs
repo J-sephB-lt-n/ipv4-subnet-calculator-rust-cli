2eb541b8c37b5aa9d37e56f6f28a898da6236502
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-12-22 22:26:24 +0200

    feat(src/main.rs): binary numbers working sortof

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,30 @@ fn main() {
     let (octets, cidr_num) = parse_user_cidr_input(&user_cidr_input);
 
     let num_avail_ip_addresses: u32 = 2u32.pow(32u32 - cidr_num);
-    println!("There are {} IP addresses in this subnet", num_avail_ip_addresses);
+    println!(
+        "There are {} IP addresses in this subnet",
+        num_avail_ip_addresses
+    );
     println!("{} IP addresses are assignable to hosts (1 IP is reserved for network address and 1 for broadcast address)", num_avail_ip_addresses-2);
-    println!("{:#?}", &octets);
-    println!("{}", &cidr_num);
+    let binary_num_str = octets_to_binary_num_str(&octets);
+    let start_ip_str = format!("{:0<32}", &binary_num_str[..cidr_num as usize]);
+    let end_ip_str = format!("{:1<32}", &binary_num_str[..cidr_num as usize]);
+    println!(
+        "Subnet start IP: {}.{}.{}.{}\nSubnet end IP: {}.{}.{}.{}",
+        &start_ip_str[0..8],
+        &start_ip_str[8..16],
+        &start_ip_str[16..24],
+        &start_ip_str[24..32],
+        &end_ip_str[0..8],
+        &end_ip_str[8..16],
+        &end_ip_str[16..24],
+        &end_ip_str[24..32]
+    );
 }
 
 fn parse_user_cidr_input(user_cidr_input: &str) -> ([u8; 4], u32) {
-    let parse_subnet_regex = Regex::new(r"(\d{1,3}).(\d{1,3}).(\d{1,3}).(\d{1,3})\/(\d{1,2})").unwrap();
+    let parse_subnet_regex =
+        Regex::new(r"(\d{1,3}).(\d{1,3}).(\d{1,3}).(\d{1,3})\/(\d{1,2})").unwrap();
     let subnet_captures = parse_subnet_regex.captures(&user_cidr_input).unwrap();
     let octets: [u8; 4] = [1, 2, 3, 4].map(|i| {
         subnet_captures
@@ -40,4 +56,12 @@ fn parse_user_cidr_input(user_cidr_input: &str) -> ([u8; 4], u32) {
         .expect("Invalid CIDR");
 
     (octets, cidr_num)
+}
+
+fn octets_to_binary_num_str(octets: &[u8; 4]) -> String {
+    let binary_num_str: String = octets
+        .iter()
+        .map(|octet| format!("{:08b}", octet))
+        .collect();
+    binary_num_str
 }
