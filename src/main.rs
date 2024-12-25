d6576d4efa54ae383e098c9222322d09b1a31ce8
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-12-25 20:13:07 +0200

    test(src/main.rs): added unit tests. This project is finished

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,8 @@ fn main() {
     );
     println!("{} IP addresses are assignable to hosts (1 IP is reserved for network address and 1 for broadcast address)", num_avail_ip_addresses-2);
 
-    let binary_num_str = octets_to_binary_num_str(&octets);
-    let network_ip_str = format!("{:0<32}", &binary_num_str[..cidr_num as usize]);
-    let network_ip_int = u32::from_str_radix(&network_ip_str, 2).unwrap();
-    let min_ip_int: u32 = network_ip_int + 1;
-    let min_ip_str = format!("{:032b}", min_ip_int);
-    let broadcast_ip_str = format!("{:1<32}", &binary_num_str[..cidr_num as usize]);
-    let broadcast_ip_int = u32::from_str_radix(&broadcast_ip_str, 2).unwrap();
-    let max_ip_int: u32 = broadcast_ip_int - 1;
-    let max_ip_str = format!("{:032b}", max_ip_int);
+    let (network_ip_str, min_ip_str, max_ip_str, broadcast_ip_str) =
+        subnet_calcs(&octets, cidr_num);
 
     println!(
         "Network IP: {} ({})",
@@ -100,4 +93,97 @@ fn binary_subnet_str_to_base10_octets(subnet_str: &str) -> String {
         .map(|octet| u8::from_str_radix(octet, 2).unwrap().to_string())
         .collect();
     octets.join(".")
+}
+
+fn subnet_calcs(octets: &[u8; 4], cidr_num: u32) -> (String, String, String, String) {
+    let binary_num_str = octets_to_binary_num_str(&octets);
+    let network_ip_str = format!("{:0<32}", &binary_num_str[..cidr_num as usize]);
+    let network_ip_int = u32::from_str_radix(&network_ip_str, 2).unwrap();
+    let min_ip_int: u32 = network_ip_int + 1;
+    let min_ip_str = format!("{:032b}", min_ip_int);
+    let broadcast_ip_str = format!("{:1<32}", &binary_num_str[..cidr_num as usize]);
+    let broadcast_ip_int = u32::from_str_radix(&broadcast_ip_str, 2).unwrap();
+    let max_ip_int: u32 = broadcast_ip_int - 1;
+    let max_ip_str = format!("{:032b}", max_ip_int);
+    (network_ip_str, min_ip_str, max_ip_str, broadcast_ip_str)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_end_to_end() {
+        #[derive(PartialEq)]
+        struct TestCase {
+            user_input: String,
+            expected_output: ExpectedOutput,
+        }
+        #[derive(PartialEq)]
+        struct ExpectedOutput {
+            network_ip: String,
+            host_min_ip: String,
+            host_max_ip: String,
+            broadcast_ip: String,
+        }
+        let test_cases = [
+            TestCase {
+                user_input: "132.155.18.152/9".to_string(),
+                expected_output: ExpectedOutput {
+                    network_ip: "132.128.0.0".to_string(),
+                    host_min_ip: "132.128.0.1".to_string(),
+                    host_max_ip: "132.255.255.254".to_string(),
+                    broadcast_ip: "132.255.255.255".to_string(),
+                },
+            },
+            TestCase {
+                user_input: "149.59.23.77/17".to_string(),
+                expected_output: ExpectedOutput {
+                    network_ip: "149.59.0.0".to_string(),
+                    host_min_ip: "149.59.0.1".to_string(),
+                    host_max_ip: "149.59.127.254".to_string(),
+                    broadcast_ip: "149.59.127.255".to_string(),
+                },
+            },
+            TestCase {
+                user_input: "0.255.0.255/1".to_string(),
+                expected_output: ExpectedOutput {
+                    network_ip: "0.0.0.0".to_string(),
+                    host_min_ip: "0.0.0.1".to_string(),
+                    host_max_ip: "127.255.255.254".to_string(),
+                    broadcast_ip: "127.255.255.255".to_string(),
+                },
+            },
+            TestCase {
+                user_input: "191.62.124.195/30".to_string(),
+                expected_output: ExpectedOutput {
+                    network_ip: "191.62.124.192".to_string(),
+                    host_min_ip: "191.62.124.193".to_string(),
+                    host_max_ip: "191.62.124.194".to_string(),
+                    broadcast_ip: "191.62.124.195".to_string(),
+                },
+            },
+        ];
+        for test_case in test_cases.iter() {
+            let (octets, cidr) = parse_user_cidr_input(&test_case.user_input);
+            let (network_ip_str, min_ip_str, max_ip_str, broadcast_ip_str) =
+                subnet_calcs(&octets, cidr.into());
+            assert_eq!(
+                binary_subnet_str_to_base10_octets(&network_ip_str),
+                test_case.expected_output.network_ip,
+            );
+            assert_eq!(
+                binary_subnet_str_to_base10_octets(&min_ip_str),
+                test_case.expected_output.host_min_ip,
+            );
+            assert_eq!(
+                binary_subnet_str_to_base10_octets(&max_ip_str),
+                test_case.expected_output.host_max_ip,
+            );
+            assert_eq!(
+                binary_subnet_str_to_base10_octets(&broadcast_ip_str),
+                test_case.expected_output.broadcast_ip,
+            );
+        }
+    }
 }
